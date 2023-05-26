9cb380a62a271a9f65913513e10affafb5d60e9d
powerfulh <68536042+powerfulh@users.noreply.github.com>
2023-05-26 22:46:43 +0900

    시작

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <router-view />
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from "vue";
+
+const list = ref([])
+
+list.value.push({
+	name: 'a',
+	bool: false
+})
+list.value.push({
+	name: 'b',
+	bool: false
+})
+</script>
+
+<template>
+	<div>
+		<span v-for="(item, i) in list" :key="i" :class="item.bool ? 'call' : ''">{{item.name}}</span>
+		<hr>
+		<button @click="list[0].bool = !list[0].bool">Toggle 0</button>
+		<button @click="list[1].bool = !list[1].bool">Toggle 1</button>
+	</div>
+</template>
+
+<style scoped>
+span.call {
+	font-size: 24px;
+}
+</style>
