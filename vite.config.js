77f3dcb3e211b2c3255ccb2ee44280250f8f0756
powerfulh <68536042+powerfulh@users.noreply.github.com>
2024-02-16 19:52:02 +0900

    config 빼돌리기

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from "path";
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src")
-        }
-    }
-})
+export default defineConfig(({ mode }) => ({
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	define: {
+		__APP_ENV__: loadEnv(mode, process.cwd()).APP_ENV,
+	},
+	//server: {
+	//	port: 80,
+	//},
+	// preview: {
+	//  port: 80
+	// },
+	// build: {
+	//  outDir: 'frontend'
+	// }
+}))
