a22ce6655634b1c8e6bf423e368b5484659c45e0
powerfulh <68536042+powerfulh@users.noreply.github.com>
2024-06-08 23:01:03 +0900

    기본 골자

--- src/components/PowerfulSelect.vue
+++ src/components/PowerfulSelect.vue
@@ -1,17 +1,97 @@
 <script setup>
-defineProps({
+import { ref, watch } from 'vue'
+
+const prop = defineProps({
 	/** @type import('vue').PropType<Array<SelectItem>> */
 	list: {
 		type: Array,
 		required: true,
 	},
+	modelValue: {
+		type: [String, Number],
+		required: true,
+	},
+	//defaultIndex: {
+	//	type: Number,
+	//	default: 0,
+	//},
+	readonly: Boolean,
+	disabled: Boolean,
+	multi: Boolean,
+	autoComp: Boolean,
 })
+const emit = defineEmits(['update:model-value'])
+
+const inputText = ref('')
+
+function focusEnter() {
+	if (prop.list.length) ulDom.value.children[0].focus()
+}
+function onFocus({ target }) {
+	if (prop.autoComp != true) target.blur()
+	focusEnter()
+}
+/** @param {SelectItem} item */
+function selectItem(item) {
+	emit('update:model-value', item.code)
+	inputText.value = item.text
+}
+/** @param {SelectItem} item */
+function onItemClick(item, target) {
+	if (prop.multi) {
+		// todo
+	} else {
+		selectItem(item)
+		target.blur()
+	}
+}
+function init() {
+	if (prop.list.length) selectItem(prop.list[0])
+}
+
+init()
+
+// After mount
+const ulDom = ref()
+const inputDom = ref()
+
+watch(prop.list, init)
+watch(
+	() => prop.modelValue,
+	nv => {
+		inputText.value = prop.list.find(item => item.code == nv)?.text || 'Invalid model'
+	},
+)
 </script>
 
 <template>
-	<div>
-		<select>
+	<div class="powerful-select-wrap">
+		<select :value="modelValue">
 			<option v-for="(item, i) in list" :key="i" :value="item.code">{{ item.text }}</option>
 		</select>
+		<input v-model="inputText" autocomplete="off" :readonly="readonly" :disabled="disabled" ref="inputDom" @focus="onFocus" />
+		<!--<button>right btn</button>-->
+		<ul v-show="readonly != true" ref="ulDom">
+			<li v-for="(item, i) in list" :key="i" tabindex="1" @click="({ target }) => onItemClick(item, target)">
+				{{ item.text }}
+			</li>
+		</ul>
 	</div>
 </template>
+
+<style lang="scss" scoped>
+div.powerful-select-wrap {
+	> select {
+		display: none;
+	}
+	&:has(input:focus) > ul,
+	> ul:has(li:focus) {
+		position: absolute;
+		left: 0;
+	}
+	> ul {
+		position: fixed;
+		left: -100%;
+	}
+}
+</style>
--- src/views/PowerfulSelect.vue
+++ src/views/PowerfulSelect.vue
@@ -1,21 +1,59 @@
 <script setup>
+import { reactive, ref } from 'vue'
 import PS from '@/components/PowerfulSelect.vue'
 
-const testData = [
-	{
-		code: 1,
-		text: 'a',
-	},
-	{
-		code: 2,
-		text: 'b',
-	},
-]
+const testData = reactive([])
+const selected = ref('')
+
+setTimeout(() => {
+	console.log('set')
+	testData.length = 0
+	testData.push(
+		...[
+			{
+				code: 1,
+				text: 'a',
+			},
+			{
+				code: 2,
+				text: 'b',
+			},
+		],
+	)
+}, 1500)
 </script>
 
 <template>
 	<main>
 		<h1>Powerful Select</h1>
-		<p-s :list="testData"></p-s>
+		<li>
+			<div class="label">a</div>
+			<input type="text" />
+		</li>
+		<li>
+			<div class="label">b</div>
+			<p-s v-model="selected" :list="testData"></p-s>
+		</li>
+		{{ selected }}
+		<button @click="selected = 3">test</button>
 	</main>
 </template>
+
+<style lang="scss">
+div.label {
+	display: inline-block;
+	width: 20px;
+}
+div.powerful-select-wrap {
+	display: inline-block;
+	position: relative;
+	> ul {
+		background-color: darkblue;
+		width: 100%;
+		padding-inline-start: 0;
+		> li:hover {
+			background-color: darkcyan;
+		}
+	}
+}
+</style>
